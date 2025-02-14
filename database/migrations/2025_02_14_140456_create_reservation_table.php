055192b43c1d8e049397b845412640bc918e25c9
floppersNoob <donnaguilar12@gmail.com>
2025-02-14 22:19:51 +0800

    SecondTask

---
+++ database/migrations/2025_02_14_140456_create_reservation_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->id('reservation_id');
+            $table->foreignId('guest_id');
+            $table->dateTime('check_in');
+            $table->dateTime('check_out');
+            $table->timestamps();
+
+            $table->foreign('guest_id')->references('guest_id')->on('guest')->onDelete('cascade')->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservation');
+    }
+};
---
+++ database/migrations/2025_02_14_140510_create_payment_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id('payment_id');
+            $table->foreignId('reservation_id');
+            $table->string('mode_of_payment');
+            $table->timestamps();
+
+
+            $table->foreign('reservation_id')->references('reservation_id')->on('reservation')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment');
+    }
+};
